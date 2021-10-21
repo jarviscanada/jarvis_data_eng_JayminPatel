fc502e4189d5d2cded14ceb2f16bcc3d5b4538c7
Jaymin Patel <jayminpatel1844@gmail.com>
2021-10-21 18:15:12 +0000

    Scripts

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,36 +1,36 @@
-SELECT cpu_number,
-       id,
-       total_mem,
-       row_number()  OVER
-    (
-        PARTITION BY cpu_number
-        ORDER BY total_mem DESC
-    )
-FROM host_info;
+SELECT cpu_number, id AS host_id, total_mem
 
--- round timestamp = 5 min
-CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
-    $$
-BEGIN
-RETURN date_trunc('hour', ts) + date_part('minute', ts):: int / 5 * interval '5 min';
+FROM host_info
+
+ORDER BY cpu_number ASC, total_mem DESC;
+
+CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS $$ BEGIN RETURN
+    date_trunc('hour', ts) + date_part('minute', ts):: int / 5 * interval '5 min';
 END;
 $$
-LANGUAGE PLPGSQL;
+
+SELECT usg.host_id,
+    hostname AS host_name,
+    round5(usg.timestamp) as timestamp,
+    AVG(((total_mem::float - memory_free::float)/total_mem::float) * 100)::int AS avg_used_mem_percentage
+
+FROM host_info AS inf, host_usage AS usg
+
+WHERE inf.id = usg.host_id
+
+GROUP BY usg.host_id,
+    hostname, round5(usg.timestamp)
+
+ORDER BY timestamp ASC;
 
 SELECT host_id,
-       round5( host_usage.timestamp ),
-       trunc( avg( host_info.total_mem-memory_free ) / avg( host_info.total_mem ) * 100,1)
-           as avg_used_memory_percentage
-FROM host_usage
-         LEFT JOIN host_info
-                   ON host_usage.host_id=host_info.id
-group by round5, host_id
-order by host_id;
+    round5(timestamp), count(*) AS num_data_points
 
-SELECT host_id,
-       round5(host_usage.timestamp),
-       COUNT(host_id) AS num_data_points
 FROM host_usage
-GROUP BY host_id, round5
-HAVING count(host_id)<=2
-ORDER BY host_id, round5;
+
+GROUP BY
+    host_id, round5(timestamp)
+
+HAVING count(*) < 3
+
+ORDER BY host_id, round5(timestamp);
